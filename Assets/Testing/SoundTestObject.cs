c6ce2b5b30e353242e2b002cf31491859ab319c0
Joel v H <joel@embeddedfitness.nl>
2019-07-12 17:03:12 +0200

    Language testing

---
+++ Assets/Testing/SoundTestObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTestObject : MonoBehaviour
+{
+    public AudioSource As;
+    private void OnMouseDown()
+    {
+        As.Play();
+    }
+}
