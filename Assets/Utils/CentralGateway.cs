801a8b9d0b87ec4addd29923309d62d2cdc38161
Joel v H <joel@embeddedfitness.nl>
2019-07-12 13:44:28 +0200

    Language
    Added test scene
    Added cantralgateway
    added language files

---
+++ Assets/Utils/CentralGateway.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralGateway : MonoBehaviour
+{
+    [HideInInspector]
+    public CentralGateway Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("CentralGateway | Awake | A second CentralGateway was loaded, maybe you should make a initialisation scene?");
+            Destroy(this);
+        }
+    }
+}
