2ede2cda76094fa355b54ff10fe31fef207ae7b7
Sven Janssen <svenjanssen12321@gmail.com>
2022-09-22 16:58:57 +0200

    Made some changes tp SWButton

---
+++ Assets/Editor/SWButtonGUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(SWButton))]
+public class SWButtonGUI : ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SWButton SWButton = (SWButton)target;
+
+        //SWButton.Graphics = EditorGUILayout.PropertyField();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("Graphics"), true);
+        serializedObject.ApplyModifiedProperties();
+        SWButton.Pixels = EditorGUILayout.IntField("Pixels", SWButton.Pixels);
+
+    }
+}
--- Assets/SmartwallPackage/Utils/SWButton.cs
+++ Assets/SmartwallPackage/Utils/SWButton.cs
@@ -2,17 +2,22 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(Collider2D))]
+[AddComponentMenu("UI/Button", 30)]
 public class SWButton : Button, I_SmartwallInteractable
 {
+    public List<RectTransform> Graphics;
+    public int Pixels;
+
     private bool Cooldown = false;
 
-#if UNITY_EDITOR
-    public void Hit(Vector3 hitpos){ }
+#if !UNITY_EDITOR
+    public void Hit(Vector3 hitpos) { }
 #endif
 
-#if !UNITY_EDITOR
+#if UNITY_EDITOR
     public void Hit(Vector3 location)
     {
         if (!Cooldown)
@@ -26,12 +31,25 @@ public class SWButton : Button, I_SmartwallInteractable
         Cooldown = true;
 
         ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+        MoveGraphics(-Pixels);
+
         yield return new WaitForSeconds(0.1f);
         ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
         yield return new WaitForSeconds(0.1f);
+
         ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerUpHandler);
+        MoveGraphics(Pixels);
 
         Cooldown = false;
     }
+
+    private void MoveGraphics(int direction)
+    {
+        foreach (RectTransform graphic in Graphics)
+        {
+            graphic.rect.Set(graphic.rect.x, graphic.rect.y + direction, graphic.rect.width, graphic.rect.height);
+        }
+    }
 #endif
 }
+    
